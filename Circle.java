d02f2db26bba672b3ccb943d9e3ccbf75cd13e41
kszafraniec1 <89156622+kszafraniec1@users.noreply.github.com>
2021-09-13 22:29:06 -0500

    Finished projects

--- Circle.java
+++ Circle.java
@@ -2,7 +2,7 @@
 /**
  * Write a description of class Circle here.
  *
- * @author (your name)
+ * Kevin Szafraniec
  * @version (a version number or a date)
  */
 import java.util.Scanner;
@@ -19,7 +19,9 @@ public class Circle
         DecimalFormat format = new DecimalFormat("0.####");
         System.out.println("What is the circles radius: ");
         r = Keyboard.nextInt();
+        //finds area which is PI*r^2
         area=Math.PI*Math.pow(r,2);
+        //finds circumference
         circumference= 2*Math.PI*r;
         System.out.println("The circle's area is "+format.format(area));
         System.out.println("The circle's circumference is "+format.format(circumference));
--- Coin.java
+++ Coin.java
@@ -1,16 +1,44 @@
-
 /**
  * Write a description of class Coin here.
  *
- * @author (your name)
+ * Kevin Szafraniec
  * @version (a version number or a date)
  */
 import java.util.Scanner;
+import java.text.NumberFormat;
 public class Coin
 {
     public static void main(String[] args)
     {
         Scanner Keyboard = new Scanner(System.in);
-        System.out.println("How many");
+        NumberFormat money = NumberFormat.getCurrencyInstance();
+        double quarterT;
+        double dimeT;
+        double nickelT;
+        double pennieT;
+        double Total;
+        
+        System.out.println("\nHow many quarters do you have ");
+        int quarter = Keyboard.nextInt();
+        
+        System.out.println("How many dimes do you have ");
+        int dime = Keyboard.nextInt();
+        
+        System.out.println("How many nickels do you have ");
+        int nickels = Keyboard.nextInt();
+        
+        System.out.println("How many pennies do you have ");
+        int pennies = Keyboard.nextInt();
+        
+        //Calculating coin values
+        quarterT=quarter*.25;
+        dimeT=dime*.1;
+        nickelT=nickels*.05;
+        pennieT=pennies*.01;
+        Total=quarterT+dimeT+nickelT+pennieT;
+        //displaying currency format
+        System.out.println("\nYour total is: "+money.format(Total));
+        
+        
     }
 }
